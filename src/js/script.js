d492e5450e58369a65fbbfa66fd7aff5cc3a1ee7
Derik Nascimento <79671639+Derik540@users.noreply.github.com>
2023-12-08 17:53:32 -0300

    mod

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 function addTask() {
   const inputText = document.getElementById("input-text");
+
   const taskList = document.getElementById("task-list");
   const listItem = document.createElement("li");
   listItem.textContent = inputText.value;
